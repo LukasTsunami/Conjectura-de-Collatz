692f986d1a11a49cf7a2332fec139bc775e61134
Lucas Caponi Da Silva <974900@PPC01INFLL00404.pucminas.net>
2019-03-28 20:04:20 -0300

    Parallelizing code

--- collatz.c
+++ collatz.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpi.h"
 
 struct Sequence{
    long maximum;
@@ -10,9 +11,43 @@ long collatz_conjecture(long number, long counter);
 struct Sequence get_maximum_sequence(long number);	
 
 void main(){
-	long test_number = 111111;
-	struct Sequence some_sequence = get_maximum_sequence(test_number);
-	printf("\nGreater Sequence = {%d} - Which number generate the greatest sequence = {%d}\n",some_sequence.maximum, some_sequence.number_that_generates_the_greatest_sequence);
+   int ret, rank, size, tag = 0;
+	MPI_Status status;
+   ret = MPI_Init(&argc, &argv);
+   ret = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   ret = MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank == 0){
+			int initial = 1;
+			long test_number = 111111;
+			struct Sequence some_sequence = get_maximum_sequence(initial, test_number);
+			long ret1 = MPI_Recv(&variavel_transportada, 1, MPI_LONG, 1, tag, MPI_COMM_WORLD, &status);
+			long ret2 = MPI_Recv(&variavel_transportada, 1, MPI_LONG, 2, tag, MPI_COMM_WORLD, &status);
+			long ret3 = MPI_Recv(&variavel_transportada, 1, MPI_LONG, 3, tag, MPI_COMM_WORLD, &status);
+			printf("ret0, ret1 %d ret2 %d ret3 %d",some_sequence.maximum, ret1, ret2, ret3);
+	}
+	else if (rank == 1){
+			int initial = 111112;
+			long test_number = 222222;
+			struct Sequence some_sequence = get_maximum_sequence(initial, test_number);
+			long maximum = some_sequence.maximum;
+        	ret = MPI_Send(&maximum, 1, MPI_LONG, 0, tag, MPI_COMM_WORLD);
+	}else if (rank == 2){
+			int initial = 222223;
+			long test_number = 333333;
+			struct Sequence some_sequence = get_maximum_sequence(initial, test_number);
+			long maximum = some_sequence.maximum;
+        	ret = MPI_Send(&maximum, 1, MPI_LONG, 0, tag, MPI_COMM_WORLD);
+	}else if (rank == 3){
+			int initial = 333334;
+			long test_number = 444445;
+			struct Sequence some_sequence = get_maximum_sequence(initial, test_number);
+			long maximum = some_sequence.maximum;
+        	ret = MPI_Send(&maximum, 1, MPI_LONG, 0, tag, MPI_COMM_WORLD);
+	}
+
+	novo_tempo = MPI_Wtime()-tempo_atual;
+	ret = MPI_Finalize();
 }
 
 
@@ -23,7 +58,7 @@ long collatz_conjecture(long number, long counter){
 	collatz_conjecture(new_number, counter);
 }
 
-struct Sequence get_maximum_sequence(long number){
+struct Sequence get_maximum_sequence(long initial, long number){
 	long current_number = 1;
 	long number_that_generates_the_greatest_sequence = 0;
 	long maximum = -1;
