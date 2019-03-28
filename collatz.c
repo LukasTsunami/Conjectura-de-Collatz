9af53cbf4c822be70170ff1b1e72b0d48ae1fa11
Lucas Caponi Da Silva <974900@PPC01INFLL00404.pucminas.net>
2019-03-28 19:34:11 -0300

    Changing variables to long type, renaming variables and refactoring code positions

--- collatz.c
+++ collatz.c
@@ -1,41 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Metadata{
-   int maximum;
-   int number_that_generates_the_greatest_sequence;
+struct Sequence{
+   long maximum;
+   long number_that_generates_the_greatest_sequence;
 };
 
+long collatz_conjecture(long number, long counter);
+struct Sequence get_maximum_sequence(long number);	
+
 void main(){
-	int collatz_conjecture(int number, int counter){
-		if(number == 1) return counter;
-		counter++;
-		int new_number = (number % 2==0) ? number/2 : number * 3 + 1;
-		collatz_conjecture(new_number, counter);
-	}
-
-
-	struct Metadata get_maximum_sequence(int number){
-		int current_number = 1;
-		int number_that_generates_the_greatest_sequence = 0;
-		int maximum = -1;
-		for(; current_number <= number; current_number++){
-		   int counter = 0;
-		   int returnn = collatz_conjecture(current_number, counter);
-
-		   if(returnn > maximum){
-			maximum = returnn;
-			number_that_generates_the_greatest_sequence = current_number;
-		   }
-		   //printf("\ncurrent [%d] contador [%d] | maximum [%d]\n",current_number,returnn,maximum);
-		}
-		struct Metadata aux;
-		aux.maximum = maximum;
-		aux.number_that_generates_the_greatest_sequence = number_that_generates_the_greatest_sequence;
-		return aux;
-	}
-
 	long test_number = 111111;
-	struct Metadata metaDataQualquer = get_maximum_sequence(test_number);
-	printf("\nMaior numero = {%d} - Numero que gera = {%d}\n",metaDataQualquer.maximum, metaDataQualquer.number_that_generates_the_greatest_sequence);
+	struct Sequence some_sequence = get_maximum_sequence(test_number);
+	printf("\nGreater Sequence = {%d} - Which number generate the greatest sequence = {%d}\n",some_sequence.maximum, some_sequence.number_that_generates_the_greatest_sequence);
+}
+
+
+long collatz_conjecture(long number, long counter){
+	if(number == 1) return counter;
+	counter++;
+	long new_number = (number % 2==0) ? number/2 : number * 3 + 1;
+	collatz_conjecture(new_number, counter);
+}
+
+struct Sequence get_maximum_sequence(long number){
+	long current_number = 1;
+	long number_that_generates_the_greatest_sequence = 0;
+	long maximum = -1;
+	for(; current_number <= number; current_number++){
+		long counter = 0;
+		long returnn = collatz_conjecture(current_number, counter);
+
+		if(returnn > maximum){
+		maximum = returnn;
+		number_that_generates_the_greatest_sequence = current_number;
+		} 
+	}
+	struct Sequence aux;
+	aux.maximum = maximum;
+	aux.number_that_generates_the_greatest_sequence = number_that_generates_the_greatest_sequence;
+	return aux;
 }
